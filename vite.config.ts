008c57f3117bd6580721f0bfce51e92a086d7f50
Sumant-Dusane <sumant.dusane1966@gmail.com>
2025-05-04 01:25:29 +0530

    minimized for popup

--- vite.config.ts
+++ vite.config.ts
@@ -17,14 +17,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         popup: path.resolve(__dirname, "index.html"),
-        content_script: path.resolve(__dirname, "src/content/index.ts"),
       },
       output: {
-        entryFileNames: (chunkInfo) => {
-          return chunkInfo.name === "content_script"
-            ? "assets/content_script.js"
-            : "assets/[name]-[hash].js";
-        },
+        entryFileNames: "assets/[name]-[hash].js",
         chunkFileNames: "assets/[name]-[hash].js",
         assetFileNames: "assets/[name]-[hash].[ext]",
         format: "es",
